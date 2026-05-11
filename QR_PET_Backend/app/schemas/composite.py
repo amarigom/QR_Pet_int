61331f079eeed8168655da46301cca8a84027ed9
amarigom <andreamarigomez@gmail.com>
2026-05-11 11:11:54 -0300

    agregando model rebuild en composite.py

--- QR_PET_Backend/app/schemas/composite.py
+++ QR_PET_Backend/app/schemas/composite.py
@@ -37,5 +37,7 @@ class UserProfile(UserWithPets):
     model_config = ConfigDict(from_attributes=True)
 
 # Rebuilds de seguridad para resolver las referencias cruzadas
+
 PetWithOwner.model_rebuild()
 PetDetailResponse.model_rebuild()
+UserWithPets.model_rebuild()
