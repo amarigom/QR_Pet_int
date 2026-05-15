318922871d7961af7c6ee9a5f4395a3a71cd5ee8
amarigom <andreamarigomez@gmail.com>
2026-05-15 16:45:59 -0300

    arreglando CORS

--- QR_PET_Backend/app/middleware.py
+++ QR_PET_Backend/app/middleware.py
@@ -5,7 +5,7 @@ def setup_middleware(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
         # Especifica el origen exacto de tu Next.js
-        allow_origins=["http://localhost:3000"], 
+        allow_origins=["http://localhost:3000","https://qr-pet-int-frontendprueba-andreas-projects-71d69b69.vercel.app"], 
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
