59dc4fb1720729c0a6587bca24c9bce854923876
amarigom <andreamarigomez@gmail.com>
2026-05-29 22:30:36 -0300

    cambios en escaneos de qr

--- QR_PET_Backend/app/utils/qr_generator.py
+++ QR_PET_Backend/app/utils/qr_generator.py
@@ -2,23 +2,24 @@
 import os
 import qrcode
 from pathlib import Path
+from app.config import settings  
 
-# Definimos la ruta base para guardar las imágenes (por defecto en una carpeta 'outputs')
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-OUTPUT_DIR = BASE_DIR / "static" / "qrcodes"
-
-def generar_qr_medalla(id_mascota: str, url_base: str = "https://qr-pet-int-prueba2.vercel.app/scan/") -> str:
+def generar_qr_medalla(id_mascota: str, url_base: str = None) -> str:
     """
     Genera un código QR con alta tolerancia a fallos (30% de daño máximo)
-    y lo guarda en el disco.
+    y lo guarda en la ruta estática configurada en el proyecto.
     """
-    # 1. Asegurar que la carpeta de salida exista
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
     
-    # 2. Configurar la URL final que va a leer el celular
+    if url_base is None:
+        
+        base_limpia = settings.FRONTEND_URL.rstrip('/')
+        url_base = f"{base_limpia}/scan/"
+
+    os.makedirs(settings.STATIC_QR_DIR, exist_ok=True)
+    
     url_final = f"{url_base}{id_mascota}"
     
-    # 3. Configurar el generador QR con tolerancia Alta (ERROR_CORRECT_H)
+    # 4. Configurar el generador QR con tolerancia Alta (ERROR_CORRECT_H)
     qr = qrcode.QRCode(
         version=None, # None permite que el tamaño se adapte automáticamente al texto
         error_correction=qrcode.constants.ERROR_CORRECT_H, # Alta tolerancia para medallas de calle
@@ -28,12 +29,9 @@ def generar_qr_medalla(id_mascota: str, url_base: str = "https://qr-pet-int-prue
     
     qr.add_data(url_final)
     qr.make(fit=True)
-    
-    # 4. Crear la imagen (máximo contraste: negro y blanco puro)
     imagen_qr = qr.make_image(fill_color="black", back_color="white")
     
-    # 5. Guardar el archivo
-    ruta_archivo = OUTPUT_DIR / f"qr_{id_mascota}.png"
+    ruta_archivo = settings.STATIC_QR_DIR / f"qr_{id_mascota}.png"
     imagen_qr.save(ruta_archivo)
     
     return str(ruta_archivo)
@@ -41,7 +39,6 @@ def generar_qr_medalla(id_mascota: str, url_base: str = "https://qr-pet-int-prue
 
 # --- BLOQUE DE EJECUCIÓN DIRECTA (SÓLO PARA SCRIPT) ---
 if __name__ == "__main__":
-    # Este bloque solo se ejecuta si llamás al archivo directamente desde la terminal
     print("--- Generador de QR para Testeo ---")
     id_test = input("Ingresá el ID de la mascota para la prueba (ej: PET-12345): ").strip()
     
