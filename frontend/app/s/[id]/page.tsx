f888a6813d09f1e43c98531560e8fe6107df3dc3
amarigom <andreamarigomez@gmail.com>
2026-09-17 18:11:44 -0300

    Volviendo a lo que estaba.Simplify GPS access options
    Removed timeout and maximumAge options from GPS access.

--- frontend/app/s/[id]/page.tsx
+++ frontend/app/s/[id]/page.tsx
@@ -92,9 +92,7 @@ export default function PublicScanPage() {
         setLoading(false)
         toast.error("Debes permitir el acceso al GPS para ayudar")
       },
-      { enableHighAccuracy: true,
-        timeout: 8000,           // Espera máximo 8 segundos antes de lanzar error
-        maximumAge: 0}
+      { enableHighAccuracy: true}
     )
   }
 
